889b12367701f67676858578f6038bd503d5c771
saleha <saleha472@gmail.com>
2016-05-19 13:14:54 +0530

    Elmah implementations

--- MVCRegistrationform/App_Start/RouteConfig.cs
+++ MVCRegistrationform/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace MVCRegistrationform
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.IgnoreRoute("elmah.axd");
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
---
+++ MVCRegistrationform/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCRegistrationform.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult CheckError()
+        {
+            throw new Exception("This is a elmah example of a controller throwing an error");
+        }
+    }
+}
